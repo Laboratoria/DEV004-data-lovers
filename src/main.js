432764f4f24539fc7a89e2b21d2344c792b23f7b
Alexa <alexvegayard@gmail.com>
2023-01-30 08:41:30 -0500

    demostracion de como jalar achivos (main.js console.log

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+import { anotherExample } from './data.js';
+// npm start import data from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(anotherExample)
+
+const pokemonDB = data.pokemon
+
+console.log("pokemonDB =")
+console.log(pokemonDB)
+console.log("pokemonDB[0] =")
+console.log(pokemonDB[0])
+console.log("pokemonDB[0].generation =")
+console.log(pokemonDB[0].generation.name)
+console.log("pokemonDB[0].generation.name =")
+console.log(pokemonDB[0].generation.name)
