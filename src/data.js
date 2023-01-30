78b4a31e2d570770961f8f459414c479b0b246e3
thecosmeticdesigner <thecosmeticdesigner@gmail.com>
2023-01-30 07:23:36 -0600

    message

--- src/data.js
+++ src/data.js
@@ -7,3 +7,14 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+
+
+//(function () {
+// var bsa_optimize = document.createElement('script');
+//bsa_optimize.defer = true;
+//bsa_optimize.src = 'https://cdn4.buysellads.net/pub/pokemondb.js?' + (new Date() - new Date() % 600000);
+//(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(bsa_optimize);
+//})();
+
+
+
