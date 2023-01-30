cb4744efd47bb4a391b5ca8ec3f2760db7fc036c
alissasalazar <salazargamarraa@gmail.com>
2023-01-30 15:09:58 -0500

    feature-bucle-cards

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(data.pokemon);
+for(let i= 0; i< data.pokemon.length;i++){
+    console.log(data.pokemon[i].name)
+}
