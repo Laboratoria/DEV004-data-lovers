9b2c5fe1a13f2391bb1752db66491abae68b809c
katalimbu <katalina.esteban@gmail.com>
2023-02-15 00:13:06 -0300

    uni con el boton con la funcion sort

--- src/main.js
+++ src/main.js
@@ -29,7 +29,7 @@ function showTitleImage() {
 }
 showTitleImage()
 
-function ordenarReleaseDate() {
+function ordenarMasNueva() {
   const copiaGhibli = [...ghibli.films]
   copiaGhibli.sort(function (a, b) {
     if (parseInt(a.release_date) > parseInt(b.release_date)) {
@@ -42,7 +42,23 @@ function ordenarReleaseDate() {
   });
   return copiaGhibli
 }
-console.log(ordenarReleaseDate())
+console.log(ordenarMasNueva())
+
+function ordenarMasVieja() {
+  const copiaGhibli = [...ghibli.films]
+  copiaGhibli.sort(function (a, b) {
+    if (parseInt(a.release_date) > parseInt(b.release_date)) {
+      return 1;
+    }
+    if (parseInt(a.release_date) < parseInt(b.release_date)) {
+      return -1;
+    }
+    return 0;
+  });
+  return copiaGhibli
+}
+console.log(ordenarMasVieja())
+
 
 function ordenarPuntuacionMayor() {
   const copiaGhibli = [...ghibli.films]
@@ -55,10 +71,10 @@ function ordenarPuntuacionMayor() {
     }
     */
     if (parseInt(a.rt_score) > parseInt(b.rt_score)) {
-      return 1;
+      return -1;
     }
     if (parseInt(a.rt_score) < parseInt(b.rt_score)) {
-      return -1;
+      return 1;
     }
     return 0;
   });
@@ -72,10 +88,10 @@ function ordenarPuntuacionMenor() {
   
   copiaGhibli.sort((a,b) => {
     if (parseInt(a.rt_score) > parseInt(b.rt_score)) {
-      return -1;
+      return 1;
     }
     if (a.rt_score < b.rt_score) {
-      return 1;
+      return -1;
     }
     // a must be equal to b
     return 0;
@@ -83,4 +99,26 @@ function ordenarPuntuacionMenor() {
   return copiaGhibli
 }
 console.log(ordenarPuntuacionMenor())
+
+const ordenar = document.querySelector('#ordenar');
+
+ordenar.addEventListener('change', (event) => {
+if (event.target.value === "masReciente"){
+  ordenarMasNueva();
+  console.log(ordenarMasNueva())
+}
+if (event.target.value === "menosReciente"){
+  ordenarMasVieja();
+  console.log(ordenarMasVieja())
+}
+if (event.target.value === "mayorRt"){
+  ordenarPuntuacionMayor();
+  console.log(ordenarPuntuacionMayor())
+}
+if (event.target.value === "menorRt"){
+  ordenarPuntuacionMenor();
+  console.log(ordenarPuntuacionMenor())
+}
+
+});
 
