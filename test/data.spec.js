2334b0580bbad96e2ad2b40e0ccfb22618f1217d
LuzAlvaca <luz.alvaca@gmail.com>
2023-02-21 22:18:39 -0600

    primeras pruebas unitarias tobe function

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,8 +1,35 @@
-import {ordenarMasNueva} from '../src/data.js';
+import {ordenarMasNueva,ordenarMasVieja,ordenarPuntuacionMayor,ordenarPuntuacionMenor,filtrarProductor,} from '../src/data.js';
 
 
-describe('OrdenarMasNueva', () => {
-  it('deberia ser una funcion', () => {
+describe('filtrarProductor', () => {
+  it ('deberia ser una funcion', () => {
+    expect (typeof filtrarProductor ).toBe('function'); //toBe busca igualdad
+  });
+  it ('filtrar por productor', () => {
+    expect (typeof filtrarProductor ).toBe('function'); 
+  });
+});
+
+describe('funcion Sort', () => {
+  it ('deberia ser una funcion', () => {
     expect (typeof ordenarMasNueva ).toBe('function');
   });
 });
+
+describe('funcion Sort', () => {
+  it ('deberia ser una funcion', () => {
+    expect (typeof ordenarMasVieja ).toBe('function');
+  });
+});
+
+describe('funcion Sort', () => {
+  it ('deberia ser una funcion', () => {
+    expect (typeof ordenarPuntuacionMayor ).toBe('function');
+  });
+});
+describe('funcion Sort', () => {
+  it ('deberia ser una funcion', () => {
+    expect (typeof ordenarPuntuacionMenor ).toBe('function');
+  });
+});
+
