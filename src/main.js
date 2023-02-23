21c1c0cbee1f2283ec16a77b73723e73a85442ab
Carolina Paredes Arias <carolina.paredes.arias@gmail.com>
2023-02-23 18:29:15 -0300

    cambio tarjeta y CSS

--- src/main.js
+++ src/main.js
@@ -2,17 +2,19 @@ import data from "./data/pokemon/pokemon.js";
 
 console.log(data);
 const mostrarPoke = document.getElementById("mostrarPoke");
+const btn = document.getElementById("btn");
 
 ObtenerPokemon();
-
+//funcion para mostrar pokemon desde la data. nombre e imagen, que los divide en grupo
 function ObtenerPokemon() {
   data.pokemon.forEach((pokemon) => {
     mostrarPoke.innerHTML += `<div class="card">`+ 
-                                `<h6>${pokemon.name}</h6>`+
                                   `<div class="imgcard">`+ 
                                     `<img src="${pokemon.img}"></img>`+ 
                                   `</div>`+ 
+                                  `<h6>${pokemon.name}</h6>`+
                               `</div>`;
     console.log(pokemon);
   });
 }
+
