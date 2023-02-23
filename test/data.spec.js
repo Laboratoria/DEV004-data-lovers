5a6eaff7704d975cf38f1256a261b69e61e385ae
LuzAlvaca <luz.alvaca@gmail.com>
2023-02-22 19:56:50 -0600

    media querys sin completar

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,14 +1,10 @@
 import {ordenarMasVieja,ordenarPuntuacionMayor,filtrarProductor,} from '../src/data.js';
-//solo importamos las funciones puras que utilizamos (las que NO ESTAN comentada)
+//solo importamos las funciones puras que utilizamos (las que NO ESTAN comentadas)
 
 describe('filtrarProductor', () => {
   it ('deberia ser una funcion', () => {
     expect (typeof filtrarProductor ).toBe('function'); //toBe busca igualdad
-  });
-  // it ('filtrar la data por productor', () => {
-  //   const filtrado = filtrarProductor(ghibli.films, "producer",)
-  //   expect ("Hayao Miyasaki").toBe('"My Neighbor Totoro" '); 
-  // });
+  });  
 });
 
 describe('funcion Sort', () => {
