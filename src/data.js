f3f42404e850a0ea244e698f5489dac4c1e74275
Mariaisa <mdurango1130@gmail.com>
2023-03-08 11:00:54 -0500

    actualizacion

--- src/data.js
+++ src/data.js
@@ -1,9 +1,34 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
+/*export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
 };
+
+*/
+
+// estas funciones son de ejemplo
+
+//export const example = () => {
+ // return 'example';
+//};
+
+//export const anotherExample = () => {
+  //return 'OMG';
+//};
+
+const miFuncionsita = () => console.log('ahroa si sirvo 2')
+
+miFuncionsita()
+
+export {
+  miFuncionsita
+}
+
+export default {
+  a: 1,
+  b: 2
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,30 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { miFuncionsita } from "./data.js";
+import otroObjeto from "./data.js";
+import personajes from "./data/lol/lol.js";
+
+miFuncionsita()
+
+console.log('otroObjeto.a: ', otroObjeto.a)
+console.log('otroObjeto["b"]: ', otroObjeto['b'])
+
+console.log('personakes: ', personajes)
+
+
+const AatroxPartye = personajes.data.Aatrox.partype
+console.log('AatroxPartye: ', AatroxPartye)
+
+const unH1 = document.createElement('h1')
+unH1.textContent = AatroxPartye
+document.body.append(unH1)
+
+
+for (let characterName in personajes.data) {
+    const partType = personajes.data[characterName].partype
+
+    const unH1 = document.createElement('h1')
+    unH1.textContent = partType
+    document.body.append(partType, document.createElement('br'))
+}
+
 
 console.log(example, data);
