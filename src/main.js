d4cadacbc622cad81e4c99cfd2324b5afb22aa5c
Mariaisa <mdurango1130@gmail.com>
2023-03-09 20:58:04 -0500

    porfa

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import{
 
 import copyLol from './data/lol/lol.js';
 
+
 const allChampion = data.data;
 const arrObject = Object.values(allChampion);
 // console.log(allChampion);
@@ -17,3 +18,19 @@ const arrObject = Object.values(allChampion);
 
 // MOSTRAR DATA
 const container = document.getElementById('container');
+
+const createTemplate = (arr) => {
+  arr.forEach((champion) => {
+    const newElement = document.createElement('div');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    newElement.classList.add('class-div');
+    img.classList.add('class-img');
+    p.classList.add('class-name');
+    img.src = `${champion.splash}`;
+    p.innerHTML = `${champion.name}`;
+    newElement.appendChild(img);
+    newElement.appendChild(p);
+    container.appendChild(newElement);
+  });
+};
