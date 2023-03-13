1f4a8d04ccad9f46a8dc2c5faa0f32c0d6fc340e
Mariaisa <mdurango1130@gmail.com>
2023-03-13 09:57:12 -0500

    borre

--- src/data.js
+++ src/data.js
@@ -1,13 +1,1 @@
 
-export const sortData = (data, sortBy, sortOrder) => {
-  const newArray = data;
-  if (sortBy === 'name' && sortOrder === 'ascending') {
-    newArray.sort((a, b) => ((a.name > b.name) ? 1 : -1));
-  }
-  if (sortBy === 'name' && sortOrder === 'descending') {
-    newArray.sort((a, b) => ((a.name < b.name) ? 1 : -1));
-  }
-  return newArray;
-};
-
-  
