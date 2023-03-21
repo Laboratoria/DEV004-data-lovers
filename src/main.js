bd5543ff6f4e637f519a84324b1bf76fc7061154
Mariaisa <mdurango1130@gmail.com>
2023-03-21 11:10:07 -0500

    codigojs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 /*La primera línea importa un objeto 
 llamado data desde un archivo llamado lol.js en un directorio llamado data/lol.*/
-import data from "./data/lol/lol.js"; 
+import data from "./data/lol/lol.js";
 /*La segunda línea importa varias funciones 
 llamadas sortData, filterData, averageData, y averageName desde un archivo llamado data.js.*/
 //import { } from "./data.js";
@@ -8,8 +8,7 @@ llamadas sortData, filterData, averageData, y averageName desde un archivo llama
 //from "./data/lol/lol.js";
 
 const lol = data.data;
-
-//  console.log(lol)
+//console.log(lol);
 
 function printChampion(lol) {
   const root = document.getElementById("root");
@@ -17,15 +16,71 @@ function printChampion(lol) {
   for (const element in lol) {
     console.log(lol[element]);
     // template string
-    root.innerHTML += `<section class="container">
-    <h3>${lol[element].name}</h3>
-    <img src="${lol[element].splash}" alt="" class = 'lolChampion'>
-  </section>`;
+    root.innerHTML += `<section data-id="" class="container">
+      <h3>${lol[element].name}</h3>
+      <img src="${lol[element].splash}" alt="" class = 'lolChampion'>
+    </section>`;
   }
 }
 
 // Invocar funcion
+
+
 printChampion(lol);
 
+const root = document.getElementById("root");
 
+root.addEventListener("click", (event) => {
+  const championSection = event.target.closest('.container');
+  console.log(championSection)
+  if (championSection)
+  
+  {
+    
+    const id = championSection.getAttribute("data-id");
+    console.log(id)
+    const objSelectedChampio = lol[id];
+    console.log(objSelectedChampio);
+    const myModal = document.getElementById("myModal");
+    myModal.classList.remove("hide");
+    myModal.querySelector("#modalInfo").innerHTML = `
+     <p class="modalName">${objSelectedChampio.name}</p>
+     <p class="modalTitle">${objSelectedChampio.title}</p>
+     <img src="${objSelectedChampio.splash}" alt="" class="imgSplash">
+     <p class="modalTags">Rol: ${objSelectedChampio.tags}</p>
+     <div class="info">
+       <p>Defense: ${objSelectedChampio.info.defense}</p>
+       <p>attack: ${objSelectedChampio.info.attack}</p>
+       <p>magic: ${objSelectedChampio.info.magic}</p>
+       <p>difficulty: ${objSelectedChampio.info.difficulty}</p>
+     </div>
+     <div class="stats">
+       <p>life: ${objSelectedChampio.stats.hp}</p>
+       <p>Manna: ${objSelectedChampio.stats.mp}</p>
+       <p>speed: ${objSelectedChampio.stats.movespeed}</p>
+       <p>attack: ${objSelectedChampio.stats.attackrange}</p>
+     </div>
+   `;
+  }
+});
+
+//cerrar modal 
+
+//ORDENA DATA DE A a LA Z
+
+  const dataLol = {
+  Aatrox: { },
+  Zed: { },
+  // más campeones
+};
+
+ const sortedData = Object.keys(dataLol).sort((a, b) => a.localeCompare(b))
+  .reduce((acc, key) => {
+    acc[key] = dataLol[key];
+    return acc;
+  }, {});
+
+console.log(sortedData); 
+
+// FILTRAR  DATA Y CALCULAR PROMEDIO 
 
