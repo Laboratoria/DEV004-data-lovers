1bd662a39a1ea24d60da3e806178cc635e05df1b
Daniela Mateo <mateocruzdaniela@gmail.com>
2023-03-27 10:20:17 -0600

    prueba error

--- src/data.js
+++ src/data.js
@@ -1,10 +1,18 @@
-export const lol = (data)
 
-const newArray = data;
-  if (sortBy === 'name' && sortOrder === 'ascending') {
-    newArray.sort((a, b) => ((a.name > b.name) ? 1 : -1));
+
+export const campeonesMage =(lol, rol) =>{
+  const campeonesFiltrados = [];
+  for (const i in lol) {
+    if (Object.hasOwnProperty.call(lol, i)) {
+      const element = lol[i];
+      // console.log(element);
+      if(element.tags.includes(rol)){
+        campeonesFiltrados.push(element)   
+      }
+    }
   }
-  if (sortBy === 'name' && sortOrder === 'descending') {
-    newArray.sort((a, b) => ((a.name < b.name) ? 1 : -1));
-  }
-  return newArray;
+  console.log(campeonesFiltrados);
+  return campeonesFiltrados
+}
+
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 /*La primera línea importa un objeto 
 llamado data desde un archivo llamado lol.js en un directorio llamado data/lol.*/
-import data from "./data/lol/lol.js"; 
+import data from "./data/lol/lol.js";
+import { campeonesMage } from "./data.js";
 /*La segunda línea importa varias funciones 
 llamadas sortData, filterData, averageData, y averageName desde un archivo llamado data.js.*/
 //import { } from "./data.js";
@@ -13,9 +14,9 @@ const lol = data.data;
 
 function printChampion(lol) {
   const root = document.getElementById("root");
-  console.log(root);
+  // console.log(root);
   for (const element in lol) {
-    console.log(lol[element]);
+  // console.log(lol[element]);
     // template string
     root.innerHTML += `<section class="container">
     <h3>${lol[element].name}</h3>
@@ -24,8 +25,22 @@ function printChampion(lol) {
   }
 }
 
+
 // Invocar funcion
 printChampion(lol);
 
 
+
+// Filtrar solo los campeones de tipo "mage"
+const roles =  document.getElementsByClassName("rol"); // devuelve todos los elementos que coinciden con la clase en un ARREGLO
+// console.log(roles);
+
+for (let index = 0; index < roles.length; index++) {
+  // console.log(roles[index]);
+  roles[index].addEventListener('click', (e)=>{
+    const rol = e.target.id
+    campeonesMage(lol, rol)
+  })
+  
+}
 
